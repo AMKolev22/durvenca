39b69faef67c196267917da6b1778c10c554fb04
AMKolev22 <AMKolev22@codingburgas.bg>
2023-11-12 00:09:54 +0200

    Add game canvas inside of a div

--- src/components/game/main.js
+++ src/components/game/main.js
@@ -4,10 +4,11 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 scene.background = new THREE.Color(0xff0000)
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize( window.innerWidth, window.innerHeight );
 const controls = new OrbitControls(camera, renderer.domElement);
-document.body.appendChild( renderer.domElement );
+const canvas = document.getElementById('game');
+canvas.appendChild( renderer.domElement);
 
 
 
@@ -17,17 +18,17 @@ const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
-if (camera.position.z > 50){
-	camera.position.z = 5;
-}
 
 function animate() {
 	requestAnimationFrame( animate );
 	controls.update();
+	if (camera.position.z > 50){
+		camera.position.z = 5;
+	}
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
 	cube.rotation.z += 0.01;
 	renderer.render( scene, camera );
 }
 
-animate();r
+animate();
