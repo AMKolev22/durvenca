26551abad3c15e521f9f0edc9414072437caefdb
AMKolev22 <AMKolev22@codingburgas.bg>
2023-11-12 16:59:45 +0200

    Add smooth camera zoom, disable rotating. Start work on the camera

---
+++ src/components/game/camera.js
@@ -0,0 +1,2 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
--- src/components/game/main.js
+++ src/components/game/main.js
@@ -1,34 +1,77 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import {TrackballControls} from 'three/examples/jsm/controls/TrackballControls'
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-scene.background = new THREE.Color(0xff0000)
-const renderer = new THREE.WebGLRenderer({antialias: true});
-renderer.setSize( window.innerWidth, window.innerHeight );
-const controls = new OrbitControls(camera, renderer.domElement);
+const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 const canvas = document.getElementById('game');
+const renderer = new THREE.WebGLRenderer({antialias: true});
+const controls = new OrbitControls(camera, renderer.domElement);
+const controls2  = new TrackballControls(camera, renderer.domElement);
+
+const square = new THREE.BoxGeometry(1,0.1,1);
+const light = new THREE.MeshBasicMaterial({color: 0xE0C4A8})
+const dark = new THREE.MeshBasicMaterial({color: 0x6A4236})
+const cube = new THREE.Mesh(square, light)
+const board = new THREE.Group
+
+const ambl = new THREE.AmbientLight(0xFFFFFF)
+
+scene.background = new THREE.Color(0xff0000);
+renderer.setSize( window.innerWidth, window.innerHeight );
 canvas.appendChild( renderer.domElement);
 
+for (let x = 0; x < 10; x++){
+	for (let z = 0; z < 10; z++){
+		let cube;
+		if (z%2 == 0){
+			cube = new THREE.Mesh(square, x % 2 == 0 ? light : dark)
+		}
+		else{
+			cube = new THREE.Mesh(square, x % 2 == 0 ? dark : light)
+		}
+		cube.position.set(x,0,z)
+		board.add(cube)
+	}
+}
+controls.mouseButtons = {LEFT: 2, MIDDLE: 1, RIGHT: 0};
+controls.enableDamping = true;
+controls.dampingFactor = 0.2;
+controls.enableZoom = false;
+controls.enableRotate = false;
+
+controls2.noRotate = true;
+controls2.noPan = true;
+controls2.noZoom = false;
+controls.zoomSpeed = 1
 
 
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+
+
+scene.add(ambl)
+scene.add(board);
 
 camera.position.z = 5;
+camera.position.y = 20
+
+
+
 
 function animate() {
 	requestAnimationFrame( animate );
-	controls.update();
-	if (camera.position.z > 50){
-		camera.position.z = 5;
+
+	const target = controls.target
+	controls2.target.set(target.x, target.y, target.z);
+
+	if (camera.position.y > 60){
+		camera.position.y = 20;
 	}
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-	cube.rotation.z += 0.01;
+
+	
+	controls.update();
+	controls2.update();
 	renderer.render( scene, camera );
 }
 
 animate();
+
