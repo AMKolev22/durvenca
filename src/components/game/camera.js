9871570819266225e3798bb0eefcda08c3daecb4
AMKolev22 <AMKolev22@codingburgas.bg>
2023-11-17 03:54:34 +0200

    Implement dynamic camera

--- src/components/game/camera.js
+++ src/components/game/camera.js
@@ -8,15 +8,18 @@ CameraControls.install( { THREE: THREE } );
 export const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 export const controls = new CameraControls(camera, renderer.domElement);
 
-controls.enabled = true;
 controls.setPosition(0, 20, 5)
-controls.mouseButtons.right = CameraControls.ACTION.NONE;
+controls.mouseButtons.right = CameraControls.ACTION.TRUCK;
 controls.mouseButtons.left = CameraControls.ACTION.TRUCK;
 
-controls.maxZoom = 35;
-controls.minZoom = 5;
+controls.minDistance = 5
+controls.maxDistance = 30
+controls.dollyToCursor = true
+controls.smoothTime = 0.3
 
+controls.verticalDragToForward = true;
 
+controls.draggingSmoothTime = 0.135
 
 
 
--- src/components/game/main.js
+++ src/components/game/main.js
@@ -14,7 +14,10 @@ const cube = new THREE.Mesh(square, light)
 const board = new THREE.Group();
 const ambl = new THREE.AmbientLight(0xFFFFFF);
 
+let cameraPos = new THREE.Vector3();
+let boardPos = new THREE.Vector3();
 
+let topPov = true
 
 scene.background = new THREE.Color(0xff0000);
 canvas.appendChild( renderer.domElement);
@@ -25,9 +28,6 @@ grab.addEventListener('mousedown', () =>{
 grab.addEventListener('mouseup', () =>{
 	grab.style.cursor = "grab";
 })
-
-
-
 
 for (let x = 0; x < 10; x++){
 	for (let z = 0; z < 10; z++){
@@ -43,19 +43,30 @@ for (let x = 0; x < 10; x++){
 	}
 }
 
-
-
 scene.add(ambl)
 scene.add(board);
+boardPos.x = board.position.x;
+boardPos.y = board.position.y;
+boardPos.z = board.position.z;
+
+controls.getPosition(cameraPos, true)
+
 
 function animate() {
 	const delta = clock.getDelta();
+	if (cameraPos.y <= 15 && topPov == false){
+		topPov = true
+		controls.lerpLookAt(0, 20, 5, boardPos.x, boardPos.y, boardPos.z,0, 2,5, 0, 2, 0, 0.8, true)
+	}
+	else if (cameraPos.y > 5 && topPov == true){
+		controls.setLookAt(0, 20, 5, boardPos.x, boardPos.y, boardPos.z, true)
+		topPov = false
+	}
+	controls.getPosition(cameraPos, true)
+	console.log(cameraPos.y);
 	controls.update(delta)
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
 }
-
-
-
 animate();
 
