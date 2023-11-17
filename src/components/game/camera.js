ff134c175d2ad2e60668c374c9257675aca70069
AMKolev22 <AMKolev22@codingburgas.bg>
2023-11-18 01:59:16 +0200

    Fix read-only properties

--- src/components/game/camera.js
+++ src/components/game/camera.js
@@ -8,7 +8,6 @@ CameraControls.install( { THREE: THREE } );
 export const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 export const controls = new CameraControls(camera, renderer.domElement);
 
-
 controls.setPosition(0, 25, 5)
 controls.mouseButtons.right = CameraControls.ACTION.TRUCK;
 controls.mouseButtons.left = CameraControls.ACTION.TRUCK;
@@ -21,28 +20,6 @@ controls.smoothTime = 0.4
 controls.verticalDragToForward = true;
 
 controls.draggingSmoothTime = 0.1
-
-export let state = {
-    isDragging: false,
-}
-export let previousMousePosition = {
-    x: 0,
-    y: 0,
-};
-
-export function dragMouse(event){
-	controls.smoothTime = 0.4
-			controls.draggingSmoothTime = 0.03
-			if (state.isDragging) {
-				const deltaX = event.clientX - previousMousePosition.x;
-		
-				controls.rotate(deltaX * 0.0025, 0);
-		
-				previousMousePosition = {
-					x: event.clientX,
-				};
-			}
-}
 
 
 
