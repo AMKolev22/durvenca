f037b5e22ac94c7a1b844ae76a31c42a7cf504f8
AMKolev22 <AMKolev22@codingburgas.bg>
2023-11-17 05:49:22 +0200

    Add draggable movement in dynamic camera

--- src/components/game/camera.js
+++ src/components/game/camera.js
@@ -8,18 +8,18 @@ CameraControls.install( { THREE: THREE } );
 export const camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 0.1, 1000 );
 export const controls = new CameraControls(camera, renderer.domElement);
 
-controls.setPosition(0, 20, 5)
+controls.setPosition(0, 25, 5)
 controls.mouseButtons.right = CameraControls.ACTION.TRUCK;
 controls.mouseButtons.left = CameraControls.ACTION.TRUCK;
 
 controls.minDistance = 5
 controls.maxDistance = 30
 controls.dollyToCursor = true
-controls.smoothTime = 0.3
+controls.smoothTime = 0.4
 
 controls.verticalDragToForward = true;
 
-controls.draggingSmoothTime = 0.135
+controls.draggingSmoothTime = 0.1
 
 
 
--- src/components/game/main.js
+++ src/components/game/main.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three';
 import { camera, controls} from './camera';
 import { renderer } from './renderer';
+import CameraControls from 'camera-controls';
 
 
 const scene = new THREE.Scene();
@@ -16,8 +17,15 @@ const ambl = new THREE.AmbientLight(0xFFFFFF);
 
 let cameraPos = new THREE.Vector3();
 let boardPos = new THREE.Vector3();
+let orbitPos = new THREE.Vector3();
 
 let topPov = true
+
+let isDragging = false;
+let previousMousePosition = {
+    x: 0,
+    y: 0,
+};
 
 scene.background = new THREE.Color(0xff0000);
 canvas.appendChild( renderer.domElement);
@@ -48,6 +56,7 @@ scene.add(board);
 boardPos.x = board.position.x;
 boardPos.y = board.position.y;
 boardPos.z = board.position.z;
+controls.get
 
 controls.getPosition(cameraPos, true)
 
@@ -56,14 +65,45 @@ function animate() {
 	const delta = clock.getDelta();
 	if (cameraPos.y <= 15 && topPov == false){
 		topPov = true
-		controls.lerpLookAt(0, 20, 5, boardPos.x, boardPos.y, boardPos.z,0, 2,5, 0, 2, 0, 0.8, true)
+		const canvasGame = renderer.domElement;
+		controls.draggingSmoothTime = 0.2
+		controls.smoothTime = 0.4
+		controls.lerpLookAt(0, 10, 5, boardPos.x, boardPos.y, boardPos.z,0, 3,5, 0, 2, 0, 0.8, true)
+		canvasGame.addEventListener('mousedown', (event) => {
+			isDragging = true;
+			previousMousePosition = {
+				x: event.clientX,
+				y: event.clientY,
+			};
+		});
+		
+		canvasGame.addEventListener('mouseup', () => {
+			isDragging = false;
+		});
+		
+		canvasGame.addEventListener('mousemove', (event) => {
+			controls.draggingSmoothTime = 0.001
+			if (isDragging) {
+				const deltaX = event.clientX - previousMousePosition.x;
+		
+				controls.rotate(deltaX * 0.0025, 0);
+		
+				previousMousePosition = {
+					x: event.clientX,
+					y: event.clientY,
+				};
+			}
+		});
 	}
 	else if (cameraPos.y > 5 && topPov == true){
-		controls.setLookAt(0, 20, 5, boardPos.x, boardPos.y, boardPos.z, true)
+		controls.setLookAt(0, 20, 0, boardPos.x, boardPos.y, boardPos.z, true)
+		controls.draggingSmoothTime = 0.2
+		controls.smoothTime = 0.4
 		topPov = false
 	}
 	controls.getPosition(cameraPos, true)
-	console.log(cameraPos.y);
+	controls.getTarget(orbitPos)
+	console.log(cameraPos.y)
 	controls.update(delta)
 	requestAnimationFrame( animate );
 	renderer.render( scene, camera );
