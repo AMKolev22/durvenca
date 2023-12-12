7284b12db438d5f1305ce3c8a3cde3bf493dbcd8
SNRadev22 <SNRadev22@codingburgas.bg>
2023-12-12 23:35:29 +0200

    Add functionality to 'PLAY' button

--- src/site/components/hero.jsx
+++ src/site/components/hero.jsx
@@ -12,14 +12,14 @@ export default function Hero(){
         AOS.init();
         AOS.refresh();
       }, [])
-
-
     return(
         <div className="w-screen h-screen flex flex-col justify-center items-center bg-no-repeat relative " id = "hero" style={ background } data-aos-offset="15">
             <div className="flex items-center flex-col">
                 <p className="text-center font-extrabold text-[12rem] leading-tight tracking-wider" style= { textstyle } data-aos="fade-left" data-aos-duration="700" data-aos-offset="15">Explore a nature<br /> sandbox.</p>
                 <p className="text-center font-bold opacity-[0.9] leading-normal tracking-wide text-[1.5rem] pt-0" style= { textstyle } id = "small-head"><span data-aos="fade-up" data-aos-duration="700" className="inline-block">save the world,</span> <span  data-aos="fade-up" data-aos-duration="1500" className="inline-block" data-aos-offset="15">tree by tree.</span></p>
-                <a className="text-[1.8rem] font-bold opacity-[0.95] block" id = "btn-outer" data-aos="fade-right" data-aos-duration="700" data-aos-offset="15"><p style= { buttonstyle } className="px-[5.8rem] py-4 rounded-[1rem] text-[1.8rem] font-semibold" id = "btn"><span className = "ml-[2.8rem]">PLAY</span></p></a>
+                
+                <a className="text-[1.8rem] font-bold opacity-[0.95] block" id = "btn-outer" data-aos="fade-right" data-aos-duration="700" data-aos-offset="15" href="src/game/game.html"><p style= { buttonstyle } className="px-[5.8rem] py-4 rounded-[1rem] text-[1.8rem] font-semibold" id = "btn"><span className = "ml-[2.8rem]">PLAY</span></p></a>
+    
             </div>
             <div className="-mb-[5vh] mt-32">
                 <p className="text-[1.8rem] font-bold opacity-[0.8] tracking-wide mb-6" style = { textstyle }>Scroll Down</p>
