05f5c07340ee3d6ab5642f58e01e400b44509578
AMKolev22 <AMKolev22@codingburgas.bg>
2023-12-12 19:00:28 +0200

    Add loading spinner

---
+++ src/game/utils/loading.js
@@ -0,0 +1,12 @@
+const spinner = document.querySelector('.spinner-div');
+const mainContent = document.querySelector('.main');    
+window.onload = function() {
+    spinner.style.display = 'none';
+    mainContent.style.display = 'block';
+};
+
+window.addEventListener('load', function() {
+    spinner.style.display = 'none';
+    mainContent.style.display = 'block';
+  });
+  
